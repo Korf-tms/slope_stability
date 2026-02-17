ab8f662e6457ab7b7d6193c15c342b2dfbc80689
Tomáš Luber <tomas.luber@ugn.cas.cz>
2026-02-17 16:15:05 +0100

    updated generated test file

--- slope_stability/petsc-mex/mex_test_parallel.c
+++ slope_stability/petsc-mex/mex_test_parallel.c
@@ -12,86 +12,85 @@ void cleanup(void) {
 }
 
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
-    if (nrhs != 2 || !mxIsSparse(prhs[0])) {
-        mexErrMsgIdAndTxt("PETSc:Solve:InvalidInput", "Inputs: Sparse Matrix A, Vector b.");
-    }
+    PetscErrorCode ierr;
 
-    /* --- 1. INITIALIZATION (Use WORLD for potential parallelism) --- */
+    /* --- 1. INITIALIZATION --- */
     PetscInitialized(&MyPetscIsInitialized);
     if (!MyPetscIsInitialized) {
         PetscInitializeNoArguments();
-        /* Disable PETSc signal handling to protect MATLAB */
         PetscOptionsSetValue(NULL, "-no_signal_handler", "true");
         mexAtExit(cleanup);
     }
 
+    /* Check minimum inputs */
+    if (nrhs < 2 || !mxIsSparse(prhs[0])) {
+        mexErrMsgTxt("Usage: x = mex_solve(A, b, [block_size], [options_string])");
+    }
+
+    /* --- 2. PARSE OPTIONAL ARGUMENTS --- */
+    PetscInt bs = 1;
+    if (nrhs >= 3 && !mxIsEmpty(prhs[2])) {
+        bs = (PetscInt)mxGetScalar(prhs[2]);
+    }
+
+    if (nrhs >= 4 && mxIsChar(prhs[3])) {
+        char *options_buffer = mxArrayToString(prhs[3]);
+        PetscOptionsInsertString(NULL, options_buffer);
+        mxFree(options_buffer);
+    }
+
+    /* --- 3. DATA EXTRACTION --- */
     mwSize n = mxGetN(prhs[0]);
     double *vals = mxGetPr(prhs[0]);
-    mwIndex *ir = mxGetIr(prhs[0]); 
-    mwIndex *jc = mxGetJc(prhs[0]); 
+    mwIndex *ir = mxGetIr(prhs[0]);
+    mwIndex *jc = mxGetJc(prhs[0]);
     PetscInt nnz = (PetscInt)jc[n];
 
-    /* --- 2. INDEX CONVERSION --- */
     PetscInt *petsc_i, *petsc_j;
     PetscMalloc1(n + 1, &petsc_i);
     PetscMalloc1(nnz, &petsc_j);
     for (mwSize i = 0; i <= n; i++) petsc_i[i] = (PetscInt)jc[i];
     for (mwSize i = 0; i < nnz; i++) petsc_j[i] = (PetscInt)ir[i];
 
-    /* --- 3. MATRIX & VECTOR SETUP --- */
+    /* --- 4. PETSc OBJECT SETUP --- */
     Mat A;
-    /* Use PETSC_COMM_WORLD. On a single MATLAB process, size is 1. */
     MatCreateSeqAIJWithArrays(PETSC_COMM_WORLD, (PetscInt)n, (PetscInt)n, petsc_i, petsc_j, vals, &A);
+    MatSetBlockSize(A, bs);
 
     double *b_ptr = mxGetPr(prhs[1]);
     Vec b, x;
     VecCreateSeqWithArray(PETSC_COMM_WORLD, 1, (PetscInt)n, b_ptr, &b);
     VecDuplicate(b, &x);
 
-    /* --- 4. GMRES SOLVER + GAMG PRECONDITIONER --- */
+    /* --- 5. SOLVER SETUP --- */
     KSP ksp;
     KSPCreate(PETSC_COMM_WORLD, &ksp);
     KSPSetOperators(ksp, A, A);
-
-    /* Set Solver to GMRES */
-    KSPSetType(ksp, KSPGMRES);
-    KSPGMRESSetRestart(ksp, 30); // Standard restart value
-
-    /* Set Preconditioner to GAMG (Algebraic Multigrid) */
-    /* GAMG is much faster than LU/ILU for 10^5+ DOFs in 3D Elasticity */
-    PC pc;
-    KSPGetPC(ksp, &pc);
-    PCSetType(pc, PCGAMG);
-
-    /* Allow overrides from MATLAB command line (e.g., -ksp_monitor) */
     KSPSetFromOptions(ksp);
     KSPSetUp(ksp);
 
-    /* --- 5. SOLVE --- */
+    /* --- 6. SOLVE --- */
     KSPSolve(ksp, b, x);
 
+    /* --- 7. OUTPUT & CONVERGENCE INFO --- */
     KSPConvergedReason reason;
     KSPGetConvergedReason(ksp, &reason);
-    
-    PetscInt its;
-    KSPGetIterationNumber(ksp, &its);
-
-    if (reason > 0) {
-        PetscPrintf(PETSC_COMM_WORLD, "Linear solve converged in %d iterations. Reason: %s\n", 
-                    (int)its, KSPConvergedReasons[reason]);
+    if (reason < 0) {
+        PetscPrintf(PETSC_COMM_WORLD, "Solve DIVERGED with reason %d\n", (int)reason);
     } else {
-        PetscPrintf(PETSC_COMM_WORLD, "Linear solve DIVERGED in %d iterations. Reason: %s\n", 
-                    (int)its, KSPConvergedReasons[reason]);
+        PetscInt its;
+        KSPGetIterationNumber(ksp, &its);
+        PetscPrintf(PETSC_COMM_WORLD, "Solve CONVERGED in %d iterations\n", (int)its);
+    }
 
-    /* --- 6. OUTPUT --- */
     plhs[0] = mxCreateDoubleMatrix(n, 1, mxREAL);
     double *x_out = mxGetPr(plhs[0]);
     const PetscScalar *x_vals;
     VecGetArrayRead(x, &x_vals);
-    for (mwSize i = 0; i < n; i++) x_out[i] = (double)x_vals[i];
+    for (int i = 0; i < n; i++) x_out[i] = (double)x_vals[i];
     VecRestoreArrayRead(x, &x_vals);
 
-    /* --- 7. CLEANUP --- */
+    /* --- 8. CLEANUP --- */
     KSPDestroy(&ksp);
     VecDestroy(&b);
     VecDestroy(&x);
